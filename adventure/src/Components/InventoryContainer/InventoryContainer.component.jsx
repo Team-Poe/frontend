c524490ab42e686632a20f05b2929ee9a5b01105
Richard Scott Prins Jr <richardsprins@gmail.com>
2020-01-08 13:25:08 -0500

    working on frontent functionality

--- adventure/src/Components/InventoryContainer/InventoryContainer.component.jsx
+++ adventure/src/Components/InventoryContainer/InventoryContainer.component.jsx
@@ -3,14 +3,7 @@ import React from 'react'
 const InventoryContainer = () => {
   return (
     <div className="inventory">
-      <div className="item1"> Rock</div>
-      <div className="item2"> Sword</div>
-      <div className="item3"> Torch</div>
-      <div className="item4"> Rope</div>
-      <div className="item5"> Map</div>
-      <div className="item6"> Dagger</div>
-      <div className="item7"> Coin Pouch</div>
-      <div className="item8"> Lute</div>
+      <div className="item1">Inventory</div>
     </div>
   )
 }
