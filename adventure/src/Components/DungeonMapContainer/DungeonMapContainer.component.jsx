aabc18e7f33550431496a525536ff52d4b8fd545
Richard Scott Prins Jr <richardsprins@gmail.com>
2020-01-10 15:17:30 -0500

    catching up to master

--- adventure/src/Components/DungeonMapContainer/DungeonMapContainer.component.jsx
+++ adventure/src/Components/DungeonMapContainer/DungeonMapContainer.component.jsx
@@ -5,16 +5,16 @@ const DungeonMapContainer = (props) => {
   return (
     <div className="map"> 
       <div className="map-grid">
-        <div className="row-1">{rooms.filter(room => room.id <= 20 && room.id > 10).map(room => <div className="room">{room.title}</div>)}</div>
-        <div className="row-2">{rooms.filter(room => room.id <= 30 && room.id > 20).map(room => <div className="room">{room.title}</div>)}</div>
-        <div className="row-3">{rooms.filter(room => room.id <= 40 && room.id > 30).map(room => <div className="room">{room.title}</div>)}</div>
-        <div className="row-4">{rooms.filter(room => room.id <= 50 && room.id > 40).map(room => <div className="room">{room.title}</div>)}</div>
-        <div className="row-5">{rooms.filter(room => room.id <= 60 && room.id > 50).map(room => <div className="room">{room.title}</div>)}</div>
-        <div className="row-6">{rooms.filter(room => room.id <= 70 && room.id > 60).map(room => <div className="room">{room.title}</div>)}</div>
-        <div className="row-7">{rooms.filter(room => room.id <= 80 && room.id > 70).map(room => <div className="room">{room.title}</div>)}</div>
-        <div className="row-8">{rooms.filter(room => room.id <= 90 && room.id > 80).map(room => <div className="room">{room.title}</div>)}</div>
-        <div className="row-9">{rooms.filter(room => room.id <= 100 && room.id > 90).map(room => <div className="room">{room.title}</div>)}</div>
-        <div className="row-10">{rooms.filter(room => room.id <= 110 && room.id > 100).map(room => <div className="room">{room.title}</div>)}</div>
+        <div className="row-1">{rooms.filter(room => room.id <= 20 && room.id > 10).map(room => <div className="">{room.title}</div>)}</div>
+        <div className="row-2">{rooms.filter(room => room.id <= 30 && room.id > 20).map(room => <div className="">{room.title}</div>)}</div>
+        <div className="row-3">{rooms.filter(room => room.id <= 40 && room.id > 30).map(room => <div className="">{room.title}</div>)}</div>
+        <div className="row-4">{rooms.filter(room => room.id <= 50 && room.id > 40).map(room => <div className="">{room.title}</div>)}</div>
+        <div className="row-5">{rooms.filter(room => room.id <= 60 && room.id > 50).map(room => <div className="">{room.title}</div>)}</div>
+        <div className="row-6">{rooms.filter(room => room.id <= 70 && room.id > 60).map(room => <div className="">{room.title}</div>)}</div>
+        <div className="row-7">{rooms.filter(room => room.id <= 80 && room.id > 70).map(room => <div className="">{room.title}</div>)}</div>
+        <div className="row-8">{rooms.filter(room => room.id <= 90 && room.id > 80).map(room => <div className="">{room.title}</div>)}</div>
+        <div className="row-9">{rooms.filter(room => room.id <= 100 && room.id > 90).map(room => <div className="">{room.title}</div>)}</div>
+        <div className="row-10">{rooms.filter(room => room.id <= 110 && room.i0).map(room => <div className="">{room.title}</div>)}</div>
       </div>
     </div>
   )
